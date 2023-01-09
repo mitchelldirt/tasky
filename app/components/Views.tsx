6c76a5c41131d8f8330c691a6f8393edf00f60e0
Mitchell Mudd <mitchellmudd@outlook.com>
2023-01-08 20:48:19 -0500

    Updated the main ViewsMenu and consolidated my JSX into separate components 🧹

---
+++ app/components/Views.tsx
@@ -0,0 +1,65 @@
+export default function ViewsMenu() {
+  return (
+    <div className="mb-6 flex flex-col">
+      <button className="btn-wide btn justify-start rounded-b-none border-0 border-b-2 border-slate-400 text-white hover:border-slate-200">
+        <div className="flex flex-row items-center justify-start gap-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6 text-purple-400"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M8.25 6.75h12M8.25 12h12m-12 5.25h12M3.75 6.75h.007v.008H3.75V6.75zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zM3.75 12h.007v.008H3.75V12zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0zm-.375 5.25h.007v.008H3.75v-.008zm.375 0a.375.375 0 11-.75 0 .375.375 0 01.75 0z"
+            />
+          </svg>
+
+          <p className="text-white">All Tasks</p>
+        </div>
+      </button>
+      <button className="btn-wide btn justify-start rounded-none border-0 border-b-2 border-slate-400 hover:border-slate-200">
+        <div className="flex flex-row items-center justify-start gap-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6 text-yellow-400"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M12 3v2.25m6.364.386l-1.591 1.591M21 12h-2.25m-.386 6.364l-1.591-1.591M12 18.75V21m-4.773-4.227l-1.591 1.591M5.25 12H3m4.227-4.773L5.636 5.636M15.75 12a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0z"
+            />
+          </svg>
+          <p className="text-white">Today's Tasks</p>
+        </div>
+      </button>
+      <button className="btn-wide btn justify-start rounded-t-none border-0 text-white hover:border-b-2 hover:border-slate-200">
+        <div className="flex flex-row items-center justify-start gap-3">
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            stroke="currentColor"
+            className="h-6 w-6 text-green-400"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M9 12.75L11.25 15 15 9.75M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+            />
+          </svg>
+
+          <p className="text-white">Completed Tasks</p>
+        </div>
+      </button>
+    </div>
+  );
+}
--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,7 +1,8 @@
 import ProjectList from "~/components/Projects";
 import { useCatch, useLoaderData, Form, Link } from "@remix-run/react";
 import { json, redirect } from "@remix-run/node";
-
+import HomeNavBar from "~/components/HomeNavBar";
+import ViewsMenu from "~/components/Views";
 import { getUserId } from "~/session.server";
 import { getProjects } from "~/models/project.server";
 
@@ -26,102 +27,10 @@ export default function Home() {
 
   return (
     <>
-      <div className="navbar">
-        <div className="navbar-start">
-          <div className="alert alert-success bg-green-400 absolute bottom-0 left-0 w-full rounded-none shadow-lg sm:static sm:alert sm:w-fit sm:alert-success">
-            <div>
-              <span className="text-base">🚀 15 tasks completed today!</span>
-            </div>
-          </div>
-        </div>
-        <div className="navbar-center">
-          <p className="btn-ghost btn hover:bg-transparent text-xl normal-case text-green-400">
-            To.Do
-          </p>
-        </div>
-        <div className="navbar-end">
-          <button className="btn-ghost btn-circle btn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="h-8 w-8"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-              />
-            </svg>
-          </button>
-          <button className="btn-ghost btn-circle btn">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth={1.5}
-              stroke="currentColor"
-              className="h-8 w-8 text-green-400"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M12 4.5v15m7.5-7.5h-15"
-              />
-            </svg>
-          </button>
-          <div className="dropdown-end dropdown">
-            <label tabIndex={0} className="btn-ghost btn-circle avatar btn">
-              <div className="h-8 w-8 rounded-full">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  strokeWidth={1.5}
-                  stroke="currentColor"
-                  className="h-8 w-8"
-                >
-                  <path
-                    strokeLinecap="round"
-                    strokeLinejoin="round"
-                    d="M17.982 18.725A7.488 7.488 0 0012 15.75a7.488 7.488 0 00-5.982 2.975m11.963 0a9 9 0 10-11.963 0m11.963 0A8.966 8.966 0 0112 21a8.966 8.966 0 01-5.982-2.275M15 9.75a3 3 0 11-6 0 3 3 0 016 0z"
-                  />
-                </svg>
-              </div>
-            </label>
-            <ul
-              tabIndex={0}
-              className="dropdown-content menu rounded-box menu-compact mt-3 w-52 bg-base-100 p-2 shadow"
-            >
-              <li>
-                <Link to={'/profile'} className="justify-between">
-                  Profile
-                </Link>
-              </li>
-              <li>
-                <Form className="p-0" action="/logout" method="post">
-                  <button className="text-left w-full py-2 px-4">Logout</button>
-                </Form>
-              </li>
-            </ul>
-          </div>
-        </div>
-      </div>
+      <HomeNavBar />
 
       <main className="flex flex-col items-center">
-        <div className="mb-6 flex flex-col">
-          <button className="btn-wide btn justify-start rounded-b-none border-0 border-b-2 border-slate-400 text-white hover:border-slate-200">
-            🗂 &nbsp; All Tasks
-          </button>
-          <button className="btn-wide btn justify-start rounded-none border-0 border-b-2 border-slate-400 text-white hover:border-slate-200">
-            📆 &nbsp; Today's Tasks
-          </button>
-          <button className="btn-wide btn justify-start rounded-t-none border-0 text-white hover:border-b-2 hover:border-slate-200">
-            ✅ &nbsp; Completed Tasks
-          </button>
-        </div>
+        <ViewsMenu />
         <div className="w-fit rounded-md">
           <ProjectList projects={data.projects} />
         </div>
