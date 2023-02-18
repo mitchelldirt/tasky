bb17075d89a9a3f91b9692cfb886830791dacc0a
Mitchell Mudd <mitchellmudd@outlook.com>
2023-02-17 21:51:31 -0500

    Continued working on editTask and added zod for runtime checking

--- app/components/editTask.tsx
+++ app/components/editTask.tsx
@@ -1,4 +1,4 @@
-import { Link, Form } from "@remix-run/react";
+import { Link, Form, LiveReload } from "@remix-run/react";
 import type { Task } from "@prisma/client";
 import type { formError } from "~/types";
 
@@ -35,11 +35,6 @@ export default function EditTask({ ...props }: editTaskModalProps) {
             Create Project
           </h3>
           <Form method="patch">
-            <input
-              type="hidden"
-              value={props.taskContext.id}
-              name="projectId"
-            />
             {props.formError ? (
               <span className="mt-4 flex justify-center">
                 <p
@@ -53,27 +48,149 @@ export default function EditTask({ ...props }: editTaskModalProps) {
 
             <div className="form-control w-full max-w-xs">
               <label className="label">
-                <span className="label-text">Name</span>
+                <span className="label-text">
+                  Name
+                  <span className="ml-2 text-lg text-red-400">*</span>
+                </span>
               </label>
               <input
                 type="text"
+                defaultValue={props.taskContext.name}
                 placeholder="Type here"
                 className="input-bordered input w-full max-w-xs"
                 name="name"
-                defaultValue={props.taskContext.title}
+                required
                 minLength={3}
                 maxLength={27}
               />
             </div>
 
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">
+                  Project
+                  <span className="ml-2 text-lg text-red-400">*</span>
+                </span>
+              </label>
+              <select
+                defaultValue={props.taskContext.projectId || "none"}
+                name="project"
+                className="select-bordered select"
+              >
+                <option value={"none"}>NONE</option>
+                {/* {context.projects
+                  ?.filter((project) => project.id !== "none")
+                  .map((project) => {
+                    return (
+                      <option value={project.id} key={project.id}>
+                        {project.name}
+                      </option>
+                    );
+                  })} */}
+              </select>
+            </div>
+
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Description</span>
+              </label>
+              <textarea
+                className="textarea-bordered textarea h-24"
+                placeholder="Type here"
+                defaultValue={props.taskContext.description}
+                name="description"
+              ></textarea>
+            </div>
+
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Due date</span>
+              </label>
+              <input
+                type="date"
+                placeholder=""
+                className="input-bordered input w-full max-w-xs"
+                defaultValue={new Date().toISOString().slice(0, 10)}
+                name="dueDate"
+              />
+            </div>
+
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Due time</span>
+              </label>
+              <input
+                type="time"
+                placeholder=""
+                className="input-bordered input w-full max-w-xs"
+                name="dueTime"
+              />
+            </div>
+            <div className="form-control w-full max-w-xs">
+              <label className="label">
+                <span className="label-text">Priority</span>
+              </label>
+              <fieldset className="form-control w-full max-w-xs rounded-lg border-2 border-gray-400 border-opacity-20">
+                <div className="form-control ">
+                  <label className="label cursor-pointer">
+                    <span className="label-text">none</span>
+                    <input
+                      type="radio"
+                      name="priority"
+                      value={4}
+                      className="radio checked:bg-gray-400"
+                      defaultChecked
+                    />
+                  </label>
+                </div>
+
+                <div className="form-control">
+                  <label className="label cursor-pointer">
+                    <span className="label-text">low</span>
+                    <input
+                      type="radio"
+                      name="priority"
+                      value={3}
+                      className="radio checked:bg-blue-400"
+                    />
+                  </label>
+                </div>
+
+                <div className="form-control">
+                  <label className="label cursor-pointer">
+                    <span className="label-text">medium</span>
+                    <input
+                      type="radio"
+                      name="priority"
+                      value={2}
+                      className="radio checked:bg-orange-400"
+                    />
+                  </label>
+                </div>
+
+                <div className="form-control">
+                  <label className="label cursor-pointer">
+                    <span className="label-text">high</span>
+                    <input
+                      type="radio"
+                      name="priority"
+                      value={1}
+                      className="radio checked:bg-red-400"
+                    />
+                  </label>
+                </div>
+              </fieldset>
+            </div>
+
             <button
               type="submit"
-              className="btn w-full text-white hover:bg-green-400"
+              className="btn mt-4 w-full text-white hover:bg-green-400"
             >
-              Update
+              Create
             </button>
           </Form>
         </div>
+        <LiveReload />
       </div>
     </>
   );
--- app/routes/project/$projectId/task/$taskId.tsx
+++ app/routes/project/$projectId/task/$taskId.tsx
@@ -2,22 +2,59 @@ import EditTask from "~/components/editTask";
 import { useLoaderData, useOutletContext } from "@remix-run/react";
 import { getTaskById } from "~/models/task.server";
 import type { LoaderArgs } from "@remix-run/node";
+import { getProjects } from "~/models/project.server";
+import { getUserId } from "~/session.server";
+import { z } from "zod";
 
+const dataSchema = z.object({
+  task: z.object({
+    id: z.string(),
+    name: z.string(),
+    description: z.string(),
+    projectId: z.string(),
+  }),
+  projects: z.array(
+    z.object({
+      id: z.string(),
+      name: z.string(),
+      color: z.string(),
+    })
+  ),
+});
 
 export async function loader({ params, request }: LoaderArgs) {
   if (typeof params.taskId !== "string") {
     throw new Error("Uh Oh");
   }
 
+  const userId = await getUserId(request);
+
+  if (!userId) {
+    return { redirect: "/login" };
+  }
+
+  const projects = await getProjects({ userId: userId });
   const task = await getTaskById({ id: params.taskId });
-  return task;
+
+  return { task: task, projects: projects };
 }
 
 export default function EditTaskRoute() {
   const data = useLoaderData<typeof loader>();
-return (
-  <>
-  <EditTask previousRoute="/home" taskContext={data} /> 
-  </>
-)  
+
+  if (data && !isValidData(data)) {
+    return <p>Something went wrong</p>;
+  }
+
+  return (
+    <>
+    <p>HELLO</p>
+      <EditTask previousRoute="/home" taskContext={data.task} />
+    </>
+  );
+}
+
+function isValidData(input: unknown): input is z.infer<typeof dataSchema> {
+  const isValidSchema = dataSchema.safeParse(input);
+  return isValidSchema.success;
 }
