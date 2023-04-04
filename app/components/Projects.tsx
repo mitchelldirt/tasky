6eeeb565981df13255e6b9e6b4ce60b4117e48d1
mitchelldirt <mitchellmudd@outlook.com>
2023-04-04 12:30:10 -0400

    Added NoProjects and NoTasks components to be rendered when there are no projects or tasks to be displayed.

--- app/components/Projects.tsx
+++ app/components/Projects.tsx
@@ -2,6 +2,7 @@ import { createTailwindTextColor } from "~/helpers/colorClass";
 
 import type { Project } from "@prisma/client";
 import { Link } from "@remix-run/react";
+import NoProjects from "./noProjects";
 
 type projectList = {
   projects: Project[];
@@ -9,25 +10,28 @@ type projectList = {
 };
 
 export default function ProjectList({ projects, noneId }: projectList) {
-  
   return (
     <>
       <div className="flex flex-col">
-        {projects
-          .filter((project) => project.id !== noneId)
-          .map((project) => {
-            const textColorClass = createTailwindTextColor(project.color);
+        {projects && projects.length > 1
+          ? projects
+              .filter((project) => project.id !== noneId)
+              .map((project) => {
+                const textColorClass = createTailwindTextColor(project.color);
 
-            return (
-              <Link
-                className={`btn-wide btn justify-start rounded-none border-0 border-b-2 border-slate-400 first:rounded-t-md last:rounded-b-md hover:border-slate-200`}
-                key={project.id}
-                to={`/project/${project.id}`}
-              >
-                <button className={`${textColorClass}`}>{project.name}</button>
-              </Link>
-            );
-          })}
+                return (
+                  <Link
+                    className={`btn-wide btn justify-start rounded-none border-0 border-b-2 border-slate-400 first:rounded-t-md last:rounded-b-md hover:border-slate-200`}
+                    key={project.id}
+                    to={`/project/${project.id}`}
+                  >
+                    <button className={`${textColorClass}`}>
+                      {project.name}
+                    </button>
+                  </Link>
+                );
+              })
+          : <NoProjects />}
       </div>
     </>
   );
--- app/components/Tasks.tsx
+++ app/components/Tasks.tsx
@@ -1,6 +1,8 @@
 import TaskView from "./Task";
 import type { Project, Task } from "@prisma/client";
 import { LiveReload } from "@remix-run/react";
+import NoTasks from "./NoTasks";
+import { useEffect, useState } from "react";
 
 type taskList = {
   tasks: Task[] | Array<any>;
@@ -10,12 +12,20 @@ export default function Tasks({
   tasks,
   name = ""
 }: taskList & Partial<Pick<Project, "name">>) {
+
+  const [path, setPath] = useState('');
+
+  useEffect(() => { 
+    if (typeof window === "undefined") return;
+    setPath(window.location.pathname);
+  }, []);
+
   return (
     <>
       <main className="flex flex-col items-center">
         <ol className="flex w-full flex-col items-center md:w-3/4">
           {/* TODO: Refactor the below to match how completed prop works. That way you can do completed instead of completed.completed */}
-          {tasks
+          {(tasks && tasks.length > 0) || path.includes('completed') === true || path === ''
             ? tasks.map((task) => (
                 <TaskView
                   title={{ title: task.title || "Tet" }}
@@ -29,7 +39,7 @@ export default function Tasks({
                   key={task.id}
                 />
               ))
-            : null}
+            : <NoTasks />}
         </ol>
         <LiveReload />
       </main>
