af89af8a6df621642b669d0193279dbd0650e9fc
Mitchell Mudd <mitchellmudd@outlook.com>
2023-04-08 17:18:58 -0400

    Updated no projects text to be more descriptive

--- app/components/NoProjects.tsx
+++ app/components/NoProjects.tsx
@@ -15,8 +15,10 @@ export default function NoProjects() {
           d="M15.182 16.318A4.486 4.486 0 0012.016 15a4.486 4.486 0 00-3.198 1.318M21 12a9 9 0 11-18 0 9 9 0 0118 0zM9.75 9.75c0 .414-.168.75-.375.75S9 10.164 9 9.75 9.168 9 9.375 9s.375.336.375.75zm-.375 0h.008v.015h-.008V9.75zm5.625 0c0 .414-.168.75-.375.75s-.375-.336-.375-.75.168-.75.375-.75.375.336.375.75zm-.375 0h.008v.015h-.008V9.75z"
         />
       </svg>
-      <h1 className="text-white text-lg sm:text-2xl font-bold">No Projects</h1>
-      <p className="text-white text-sm sm:text-lg">Create a project to get started</p>
+      <h1 className="text-lg font-bold text-white sm:text-2xl">No Projects</h1>
+      <p className="text-sm text-white sm:text-lg">
+        Create a project to organize your tasks
+      </p>
     </div>
   );
 }
